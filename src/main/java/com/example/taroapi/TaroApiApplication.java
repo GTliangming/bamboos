de7dd89714a676ce9d3cd0ec52bf8a4bcf7d2982
liangming1 <liangming1@lightinthebox.com>
2022-05-10 10:32:47 +0800

    fix

--- src/main/java/com/example/taroapi/TaroApiApplication.java
+++ src/main/java/com/example/taroapi/TaroApiApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync  // 异步任务注解
+@EnableScheduling  // 定时任务注解
 @SpringBootApplication
 @MapperScan("com.example.taroapi.mapper")
 public class TaroApiApplication {
